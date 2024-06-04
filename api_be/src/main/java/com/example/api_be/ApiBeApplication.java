7767f1a6d2194020d42696a073b17b56d6092a79
NguyenDuongQuang <ndq1234pn@gmail.com>
2024-06-04 11:56:46 +0700

    build
    build

---
+++ api_be/src/main/java/com/example/api_be/ApiBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBeApplication.class, args);
+	}
+
+}
